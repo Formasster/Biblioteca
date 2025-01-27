c3aa20c4cba01f3a3b4c4b8b5e2f5bf13e8027ee
Formasster <amarilishenit@gmail.com>
2025-01-27 12:46:40 +0100

    Some changes with lombok

--- src/main/java/es/educastur/biblioteca2025/Controller.java
+++ src/main/java/es/educastur/biblioteca2025/Controller.java
@@ -1,5 +1,10 @@
 package es.educastur.biblioteca2025;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/Biblioteca2025")
 public class Controller {
 
 }
--- src/main/java/es/educastur/biblioteca2025/Libro.java
+++ src/main/java/es/educastur/biblioteca2025/Libro.java
@@ -1,5 +1,15 @@
 package es.educastur.biblioteca2025;
 
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Builder
 
 public class Libro {
     private String isbn;
@@ -7,48 +17,7 @@ public class Libro {
     private String autor;
     private String genero;
     private int ejemplares;
-
-    public Libro(String isbn, String titulo, String autor, String genero, int ejemplares) {
-        this.isbn = isbn;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.genero = genero;
-        this.ejemplares = ejemplares;
-    }
     
-
-    public String getIsbn() {
-        return isbn;
-    }
-    public String getTitulo() {
-        return titulo;
-    }
-    public String getAutor() {
-        return autor;
-    }
-    public String getGenero() {
-        return genero;
-    }
-    public int getEjemplares() {
-        return ejemplares;
-    }
-
-    
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-    public void setEjemplares(int ejemplares) {
-        this.ejemplares = ejemplares;
-    }
 
     @Override
     public String toString() {
